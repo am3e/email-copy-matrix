4fb51764394da7c442328d4225c0c3a958722907
am3e <amalia.reodica@gmail.com>
2022-01-06 17:53:32 -0500

    updated with user input of email section

--- src/index.js
+++ src/index.js
@@ -5,23 +5,75 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 class Input extends React.Component {
-render () {
-  return (
-    <div className="main">
-      <div className="inputField">
-        <div>This will be where the input field it</div>
+  render () {
+    return (
+      <div className="main">
+        <div className="inputField">
+          <div>Choose an email section</div>
+        </div>
+
       </div>
-
-    </div>
-  )
-
+    )
+  }
 }
 
+//main divider of the page
+//button to add email sections, require input - use default or create email sections
+//add or delete sections
+
+//add copy options for each of the email sections
+//count of variations 
+
+class EmailSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailSections: [],
+      value: 'Choose a email template section'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    // alert('An essay was submitted: ' + this.state.value);
+    this.state.emailSections.push(this.state.value);
+    this.setState(this.state)
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <div>
+            <Input />
+          </div>
+          Email Sections:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+        {this.state.emailSections.map(section => { 
+          return <div>
+            {section}
+          </div>
+        })}
+      </>
+    );
+  }
 }
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Input />
+    <EmailSection />
   </React.StrictMode>,
   document.getElementById('root')
 );
